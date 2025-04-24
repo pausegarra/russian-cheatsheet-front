9de1b009a2e2bc99b1ca5e0157cfbddfd3d2219c
psegarra <pausegarra.b@gmail.com>
2025-04-24 20:34:53 +0200

    chore: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import {Title} from "@mantine/core";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Title>Russian Cheatsheet</Title>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Title>Russian Cheatsheet</Title>} />
+        <Route path="/alphabet" element={<h1>Alphabet</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
